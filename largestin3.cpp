aa88b44f591fbaffcbb5f1c32939eb54d924717b
pkpranav28 <50529846+pkpranav28@users.noreply.github.com>
2019-05-13 20:50:01 +0530

    Add files via upload

---
+++ largestin3.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	cout<<"Enter three numbers:";
+	cin>>a>>b>>c;
+	if((a>b)&&(a>c))
+	cout<<a<<" is largest";
+	else if((b>a)&&(b>c))
+	cout<<b<<" is largest";
+	else
+	cout<<c<<" is largest";
+}
