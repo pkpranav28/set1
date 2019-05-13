7d8ce6116eeca5d909e9ebef66a765ab9c4b2926
pkpranav28 <50529846+pkpranav28@users.noreply.github.com>
2019-05-13 21:00:53 +0530

    Add files via upload

---
+++ printhello.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int i,n;
+	cout<<"Enter a number to print Hello n times:";
+	cin>>n;
+	if(n>0)
+	{
+	for(i=0;i<n;i++)
+	cout<<"\nHello";
+	}
+	else
+	cout<<"Invalid";
+}
